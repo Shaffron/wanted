a3039d9524a1294e573f66952500de765d0f0c5d
Kim Young Jin <with.alpha.and.omega@gmail.com>
2020-10-01 03:24:50 +0900

    feat: app initialize

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_migrate import Migrate, MigrateCommand
+from flask_restplus import Api
+from flask_script import Manager
+from flask_sqlalchemy import SQLAlchemy
+
+from app.views import namespace
+
+
+# app
+app = Flask(__name__)
+app.config.from_object('config')
+app.secret_key = 'wanted_secret_key'
+
+# database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command("db", MigrateCommand)
+
+# api routing
+api = Api(app, version='1.0', title='Company Search API', description='RESTful API for coding test')
+api.add_namespace(namespace, path='/api')
