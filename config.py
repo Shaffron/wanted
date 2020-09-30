60382df2479a4b95ae67061de834b25c8f07fb49
Kim Young Jin <with.alpha.and.omega@gmail.com>
2020-10-01 03:24:07 +0900

    feat: add run scripts and config

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+DEBUG = True
+TESTING = True
+SQLALCHEMY_DATABASE_URI = os.getenv('DEFAULT_DATABASE_URI')
---
+++ db.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from app import manager
+from app.models import *
+
+if __name__ == "__main__":
+    manager.run()
