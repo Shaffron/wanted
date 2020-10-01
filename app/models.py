b4ee2efc64b78fb5988d647c69a6a5e551f88427
Kim Young Jin <with.alpha.and.omega@gmail.com>
2020-10-01 18:27:47 +0900

    feat: add Tag model

--- app/models.py
+++ app/models.py
@@ -1,25 +1,49 @@
 from dataclasses import dataclass
 
+from sqlalchemy import UniqueConstraint
+
 from app import db
 
 
 @dataclass
 class Company(db.Model):
     id: int
-    company_name_ko: str
-    company_name_en: str
-    company_name_ja: str
-    company_tag_ko: str
-    company_tag_en: str
-    company_tag_ja: str
+    company_ko: str
+    company_en: str
+    company_ja: str
+    tag_ko: str
+    tag_en: str
+    tag_ja: str
 
     id = db.Column(db.Integer, primary_key=True)
-    company_name_ko = db.Column(db.String(100), nullable=True, index=True)
-    company_name_en = db.Column(db.String(100), nullable=True, index=True)
-    company_name_ja = db.Column(db.String(100), nullable=True, index=True)
-    company_tag_ko = db.Column(db.TEXT, nullable=True)
-    company_tag_en = db.Column(db.TEXT, nullable=True)
-    company_tag_ja = db.Column(db.TEXT, nullable=True)
+    company_ko = db.Column(db.String(100), nullable=True, index=True)
+    company_en = db.Column(db.String(100), nullable=True, index=True)
+    company_ja = db.Column(db.String(100), nullable=True, index=True)
+    tag_ko = db.Column(db.TEXT, nullable=True)
+    tag_en = db.Column(db.TEXT, nullable=True)
+    tag_ja = db.Column(db.TEXT, nullable=True)
+
+    tags = db.relationship("Tag")
 
     __tablename__ = "company"
+    __table_args__ = {'mysql_collate': 'utf8_general_ci'}
+
+
+@dataclass
+class Tag(db.Model):
+    id: int
+    company_id: int
+    title: str
+    type: str
+
+    id = db.Column(db.Integer, primary_key=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+    title = db.Column(db.String(100), nullable=False, index=True)
+    type = db.Column(db.String(100), nullable=False)
+
+    parent = db.relationship("Company", back_populates="tags", lazy="joined", innerjoin=True)
+
+    UniqueConstraint(company_id, title)
+
+    __tablename__ = "tag"
     __table_args__ = {'mysql_collate': 'utf8_general_ci'}
